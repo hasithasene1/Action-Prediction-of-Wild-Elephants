7e73430254820d64b89ece1e9d14d4cc8f461e67
hasithasene1 <126343406+hasithasene1@users.noreply.github.com>
2023-06-18 12:01:44 +0530

    Video to Frames
    This was a program written to extract frames from a video to tag elephants

---
+++ Other Codes/Video to Frames/video_to_frames.py
@@ -0,0 +1,33 @@
+import cv2
+import os
+from datetime import datetime
+
+
+class VideoToFrames:
+    def __init__(self,video_name) -> None:
+        self.video_name = video_name
+        
+
+    def to_frames(self,length_min,length_sec,FPS):
+
+        now = datetime.now()
+
+        current_time = now.strftime("%H%M%S")
+        self.crop_dir = current_time
+        if not os.path.exists(self.crop_dir):
+            os.makedirs(self.crop_dir)
+
+        video = cv2.VideoCapture(self.video_name)
+        FPS = video.get(cv2.CAP_PROP_FPS)
+        print(f"FPS :{FPS}")
+
+        for i in range(int((length_min*60+length_sec)*FPS)):
+            video = cv2.VideoCapture(self.video_name)
+            video.set(cv2.CAP_PROP_POS_FRAMES, i)
+            ret, frame = video.read()
+            done = cv2.imwrite(f'{self.crop_dir}/frame_{i}.png', frame)
+            print(f'{self.crop_dir}/frame_{i}.png :{done}')
+
+
+Vid = VideoToFrames('vid.mp4')
+Vid.to_frames(0,2,FPS=30)
